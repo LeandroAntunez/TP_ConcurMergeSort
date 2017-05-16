66c9dad598a83e2963f772b86c46a03de8ef6eaa
LeandroAntunez <leandro_antunez@outlook.com>
2017-05-15 21:57:44 -0300

    Es lo que se "codeo" hoy a la tarde, la primera parte del enunciado.

---
+++ src/java/MonitorArray.java
@@ -0,0 +1,77 @@
+package java;
+
+public class MonitorArray {
+
+    private int limite = 10;
+    private int[] lista = new int[limite];
+    private int contadorPosicion = 0;
+
+
+    public synchronized int size(){ return contadorPosicion;}
+
+    public synchronized boolean isEmpty() { return contadorPosicion == 0; }
+
+    public synchronized boolean contains(int e){
+        boolean resultado = false;
+        for (int i = 0; i < this.size(); i++) {
+            if (lista[i] == e) { resultado = true; }
+        }
+        return resultado;
+    }
+
+
+    /**
+     * add que agrega un elemento al final de la lista (si no hay suficiente espacio en el
+     array debe crearse uno nuevo del doble de tamaño y copiar todos los elementos
+     contenidos en el original)
+     * @param e
+     */
+    public synchronized void add(int e) {
+        if (this.size() == this.limite) {
+            limite = limite * 2;
+            int[] listaAuxiliar = new int[limite];
+            for (int i = 0; i < this.size(); i++){
+                int numeroActual = this.lista[i];
+                listaAuxiliar[i] = numeroActual;
+            }
+            this.lista = listaAuxiliar;
+        }
+        lista[contadorPosicion] = e;
+        notifyAll();
+    }
+
+    /**
+     * @return el primer valor de la lista (sin modificarla)
+     */
+    public synchronized int peek(){
+        while (this.isEmpty()) {
+            try { wait(); }
+            catch (InterruptedException e) {  }
+        }
+        return this.lista[0];
+    }
+
+    /**
+     * @return y remueve el primer valor de la lista (si la lista esta vacia debe
+     * bloquear al thread ejecutando el metodo)
+     */
+    public synchronized int pop(){
+        while (this.isEmpty()) {
+            try { wait(); }
+            catch (InterruptedException e) {  }
+        }
+        int resultado = lista[0];
+        for (int i = 1; i < this.size(); i++){
+            int valorPosicion = lista[i];
+            lista[i - 1] = valorPosicion;
+        }
+        contadorPosicion--;
+        return resultado;
+    }
+
+
+
+
+
+
+}
