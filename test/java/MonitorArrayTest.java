d18dbebecd709f02150c6a9b4dc35acd48c26d5b
Ariel Ramirez <arielramirez20395@gmail.com>
2017-05-15 23:47:23 -0300

    Se creo el test para el ArrayMonitor

---
+++ test/java/MonitorArrayTest.java
@@ -0,0 +1,23 @@
+package java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MonitorArrayTest {
+	
+	private MonitorArray lista;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		lista = new MonitorArray();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
